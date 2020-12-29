caf83f76b7dc8eeb96d62843897eade19a9ec5c8
Jeff <morbius1st@users.noreply.github.com>
2020-12-28 19:56:24 -0800

    update.  before branch 61

---
+++ Andy/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿#region using
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+#endregion
+
+// projname: Andy
+// itemname: App
+// username: jeffs
+// created:  12/28/2020 7:31:15 PM
+
+namespace Andy
+{
+	public partial class App : Application
+	{
+
+
+	}
+}
